a1f3dd1a993f7d65bab3c159385149e3c2e80388
Alex Tait <alexander.tait@nist.gov>
2019-01-30 14:21:57 -0700

    got rid of extraneous version

--- version.py
+++
@@ -1,7 +0,0 @@
-# The short X.Y version.
-version = '0.0.1'
-# The full version, including alpha/beta/rc tags.
-release = version + ""
-
-if __name__ == '__main__':
-    print('NIST SOEN PDK v' + version)
