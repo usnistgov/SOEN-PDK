ba9c21b99943590f423950dc374ce9bf17443d4a
Alex Tait <alexander.tait@nist.gov>
2019-02-01 10:18:22 -0700

    finished SNSPD cleanup. Fixed WG width bug

--- tech/OLMAC/olmac_pcells/detectors.py
+++ tech/OLMAC/olmac_pcells/detectors.py
@@ -30,63 +30,61 @@ def wg_to_snspd(wgnw_width=0.1, wgnw_length=100, wgnw_gap=0.15,
     if numsquares_meander < 1000:
         print('Warning: Not enough squares in SNSPD meander. Clipped to 1000 from {:.1f}'.format(numsquares_meander))
         numsquares_meander=1000
-    wgnw_pitch = wgnw_width + wgnw_gap
     meander_pitch = meander_width / meander_fill_factor
     meander_length = np.sqrt(numsquares_meander * meander_width * meander_pitch)
+    wgnw_pitch = wgnw_width + wgnw_gap
+    wgnw_distance_to_edge = wg_width / 2 - wgnw_width - wgnw_gap / 2
+    if wgnw_distance_to_edge < 0:
+        print('Warning: nanowire will overhang side of waveguide by {:.3f} um'.format(-wgnw_distance_to_edge))
 
-    meander = D << pg.snspd(wire_width = meander_width, wire_pitch = meander_pitch,
-                            terminals_same_side = False, size = (meander_length,None),
-                            num_squares=numsquares_meander, layer = lys['m2_nw'])
+    numsquares_per_taper = 3  # approximate
+    D.info['num_squares'] = numsquares_meander + numsquares_wgnw - meander_length/meander_width + 3*numsquares_per_taper
+    # D.info['expected_resistance'] = D.info['num_squares']*EXPECTED_RSQ_WSI
+    D.info['wire_width'] = wgnw_width
+    D.info['length']= wgnw_length
+
+    # Geometry
+    meander = D << pg.snspd(wire_width=meander_width, wire_pitch=meander_pitch,
+                            terminals_same_side=False, size=(meander_length,None),
+                            num_squares=numsquares_meander, layer=lys['m2_nw'])
     meander.reflect(p1=(0,0), p2=(1,0))
 
-    Taper = pg.optimal_step(start_width = wgnw_width, end_width = meander_width, num_pts = 50, width_tol = 1e-3,
-                                 anticrowding_factor = 1.2, layer = lys['m2_nw'])
+    Taper = pg.optimal_step(start_width=wgnw_width, end_width=meander_width,
+                            num_pts=50, width_tol=1e-3, anticrowding_factor=1.2,
+                            layer=lys['m2_nw'])
     taper1 = D << Taper
-    taper1.connect(port = 2, destination = meander.ports[1])
+    taper1.connect(2, meander.ports[1])
 
-    wgnw = D << pg.optimal_hairpin(width = wgnw_width, pitch = wgnw_pitch, length = wgnw_length, layer = lys['m2_nw'])
-    wgnw.reflect(p1 = wgnw.ports[1].midpoint, p2 = wgnw.ports[2].midpoint)
+    wgnw = D << pg.optimal_hairpin(width=wgnw_width, pitch=wgnw_pitch, length=wgnw_length,
+                                   layer=lys['m2_nw'])
+    wgnw.connect(2, taper1.ports[1])
 
-    #
-    # wgnw.xmax = meander.xmin
-    wgnw.connect(port = 1, destination = taper1.ports[1])
+    taper2 = D << Taper
+    taper2.reflect()
+    taper2.connect(1, wgnw.ports[1])
 
+    # Electrical ports
+    exit_bend = D << pg.optimal_90deg(width=meander_width, num_pts=15,
+                                      length_adjust=1, layer=lys['m2_nw'])
+    exit_bend.connect(port=2, destination=taper2.ports[2])
+    D.add_port('wiring2', port=exit_bend.ports[1])
 
-    taper2 = D.add_ref(Taper)
-    taper2.reflect(taper2.ports[1].midpoint, taper2.ports[2].midpoint)
-    taper2.connect(port = 1, destination = wgnw.ports[2])
+    exit_taper = D << pg.optimal_step(start_width=meander_width, end_width=meander_width*4,
+                                      num_pts=50, width_tol=1e-3, anticrowding_factor=1.2,
+                                      layer=lys['m2_nw'])
+    exit_taper.connect(1, meander.ports[2])
+    D.add_port('wiring1', port=exit_taper.ports[2])
 
-    taper3 = D.add_ref(pg.optimal_step(start_width = meander_width, end_width = meander_width*4, num_pts = 50, width_tol = 1e-3,
-                     anticrowding_factor = 1.2, layer = lys['m2_nw']))
-    taper3.connect(port=1, destination = meander.ports[2])
-    bend = D << pg.optimal_90deg(width = meander_width, num_pts = 15,
-                                 length_adjust = 1, layer = lys['m2_nw'])
-    bend.connect(port=2, destination=taper2.ports[2])
-
-    ## Add the electrical ports
-    # With breakouts to vias
-    # route1 = D.add_ref(port2pad(port = taper3.ports[2], Pad = nw_pad_device, pad_offset = [nw_pad_device.size[0],0], wiring_layer = lys['m2_nw']))
-    # route2 = D.add_ref(port2pad(port = bend.ports[1], Pad = nw_pad_device, pad_offset = [nw_pad_device.size[0],nw_pad_device.size[0]/2], wiring_layer = lys['m2_nw']))
-    # D.add_port(midpoint = route1.ports[1].midpoint, width =route1.ports[1].width, orientation = 0, name = 'wiring1')
-    # p = D.add_port(port = route2.ports[1], name = 'wiring2')
-    # p.orientation = 0
-
-    ## Add optical piece and ports
-    wgnw_distance = wg_width - wgnw_width - wgnw_gap / 2
-    wg = D.add_ref(pg.compass(size = [wgnw_length + wgnw_distance, wgnw_width+wgnw_pitch+wgnw_distance*2], layer = lys['wg_deep']))
+    # Waveguide and optical ports
+    wg = D << pg.compass(size = [wgnw_length + wgnw_distance_to_edge, wg_width], layer = lys['wg_deep'])
     wg.xmax = wgnw.xmax
     wg.y = wgnw.y
 
-    D.add_port(name = 'de_edge', port = wg.ports['E'])
-    D.add_port(name = 'optical', port = wg.ports['W'])
+    D.add_port('de_edge', port=wg.ports['E'])
+    D.add_port('optical', port=wg.ports['W'])
     D.ports['de_edge'].info['is_waveguide_edge'] = True
 
     pos = D.ports['optical'].midpoint
     D.move(-1*pos)
 
-    numsquares_taper = 3
-    D.info['num_squares'] = numsquares_meander+numsquares_wgnw-meander_length/meander_width + 3*numsquares_taper
-    # D.info['expected_resistance'] = D.info['num_squares']*EXPECTED_RSQ_WSI
-    D.info['wire_width'] = wgnw_width
-    D.info['length']= wgnw_length
     return D
