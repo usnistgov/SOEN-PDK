ce688b4e71a6545880ed5a20b5f0cef7e12ef679
Alex Tait <alexander.tait@nist.gov>
2019-02-02 21:42:21 -0700

    important update: translation functions and library wrappers are no longer phidl-specific. Support for nazca, gdspy, etc. will be a matter of one liners. PCell libraries can now easily be written in heterogeneous languages

--- python/cell_translations.py
+++ python/cell_translations.py
@@ -1,24 +1,63 @@
 ''' Translation functions that take Cells/Devices in different types of languages
     and convert them to Cells/Devices in other languages
 '''
-
 import os
 import pya
 
-def anyCell_to_pyaCell(initial_cell, pya_cell, write_unboundmeth=None):
+
+def celltype_to_write_unboundmeth(celltype):
+    ''' Takes a class that represents a layout Cell and gives its class' write method.
+        (there is always some version of this, although some languages don't explicitly call it Cell)
+
+        This uses try-excepted on-the-fly importing because not everybody is going to have every language installed.
+        But if they don't, then their celltype is not going to be from that language.
+    '''
+    def not_supported_error(language_name):
+        raise NotImplementedError(('The translator does not yet support {}.\n'.format(language_name)
+                                    + 'Supported languages are: pya, phidl\n'
+                                    + 'Future support will include: gdspy, nazca, ipkiss'))
+
+    try: import pya  # ok I know we already imported this, but in the future it can also be on-the-fly (i.e. converting phidl to nazca)
+    except ImportError: pass
+    else:
+        if issubclass(celltype, pya.Cell):
+            return pya.Cell.write
+
+    try: import phidl
+    except ImportError: pass
+    else:
+        if issubclass(celltype, phidl.Device):
+            write_default = phidl.Device.write_gds
+            # we don't want that extra hierarchy layer, 'topcell', so give an extra argument to prevent it
+            return lambda *args, **kwargs: write_default(*args, **kwargs, auto_rename=False)
+
+    try: import gdspy
+    except ImportError: pass
+    else:
+        not_supported_error('gdspy')
+
+    try: import nazca
+    except ImportError: pass
+    else:
+        not_supported_error('nazca')
+
+    try: import ipkiss
+    except ImportError: pass
+    else:
+        not_supported_error('ipkiss')
+
+    raise TypeError('celltype: {} is not recognized as a layout cell object'.format(celltype.__name__))
+
+
+def anyCell_to_pyaCell(initial_cell, pya_cell):
     ''' Transfers the geometry of some initial_cell into a klayout format.
         This initial_cell can be any type of layout object, even in a different language.
+
         It must provide a way to write its geometry to a layout and have only one top cell.
-
-        write_unboundmeth is a callable thing that will take the initial_cell and a filename,
-        and save a gds at that location. This is typically a method of the initial_cell's class.
-
-        For example: if type(initial_cell) == phidl.Device, then write_unboundmeth == Device.write_gds
-
-        If write_unboundmeth is None, we will try to guess what it is based on the type of the initial_cell.
+        This function will figure out what that way is, based on the class of initial_cell.
+        The supported types and their mapping to write methods are contained in celltype_to_write_unboundmeth.
     '''
-    # if write_unboundmeth is None:
-    #     write_unboundmeth = celltype_to_write_unboundmeth(type(initial_cell))
+    write_unboundmeth = celltype_to_write_unboundmeth(type(initial_cell))
     # Save the geometry of the initial cell
     tempfile = os.path.realpath('temp_externalCell_to_pyaCell.gds')
     write_unboundmeth(initial_cell, tempfile)
@@ -32,19 +71,3 @@ def anyCell_to_pyaCell(initial_cell, pya_cell, write_unboundmeth=None):
     pya_cell.copy_tree(tempcell)
     return pya_cell
 
-
-def phidlDevice_to_pyaCell(initial_cell, pya_cell):
-    ''' The phidl version of anyCell_to_pyaCell that gets the correct write method '''
-    from phidl import Device
-    write_default = Device.write_gds
-    # we don't want that extra hierarchy layer, 'topcell', so give an extra argument to prevent it
-    write_unboundmeth = lambda *args, **kwargs: write_default(*args, **kwargs, auto_rename=False)
-    return anyCell_to_pyaCell(initial_cell, pya_cell, write_unboundmeth)
-
-
-def pyaCell_to_pyaCell(initial_cell, pya_cell):
-    ''' This is a dumb function. You would never do this.
-        It's just for the purpose of illustrating abstraction with anyCell_to_pyaCell
-    '''
-    write_unboundmeth = pya.Cell.write
-    return anyCell_to_pyaCell(initial_cell, pya_cell, write_unboundmeth)
--- tech/OLMAC/olmac_pcells/ref_layouts/myscript.py
+++
@@ -1,4 +0,0 @@
-import pya
-
-layout = pya.Layout()
-layout.read('wg_to_snspd.gds')
