205d7752d1014b3fdd7ed9667c7c8379fe825a14
Alex Tait <alexander.tait@nist.gov>
2019-06-18 02:59:54 -0600

    retracking build

--- tech/OLMAC/olmac_pcells/__init__.py
+++
@@ -1,3 +0,0 @@
-from .passives import mmi1x2
-from .superconductors import htron
-from .detectors import wg_to_snspd
--- tech/OLMAC/tests/conftest.py
+++
@@ -1,6 +0,0 @@
-import os, sys
-from os.path import realpath, dirname, join
-klayout_dot_config_dir = realpath(join(dirname(__file__), '..', '..', '..'))
-olmac_dir = join(klayout_dot_config_dir, 'tech', 'OLMAC')
-dataprep_dir = join(olmac_dir, 'dataprep')
-pcell_dir = join(olmac_dir, 'olmac_pcells')
