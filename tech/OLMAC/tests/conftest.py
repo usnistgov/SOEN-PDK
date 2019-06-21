f8f23399614b8858c6112e3b3117804b5ebc19c9
Alex Tait <alexander.tait@nist.gov>
2019-06-21 17:25:21 -0600

    self contained tests

--- tech/OLMAC/tests/conftest.py
+++ tech/OLMAC/tests/conftest.py
@@ -1,6 +1,8 @@
 import os, sys
 from os.path import realpath, dirname, join
+
 klayout_dot_config_dir = realpath(join(dirname(__file__), '..', '..', '..'))
 olmac_dir = join(klayout_dot_config_dir, 'tech', 'OLMAC')
 dataprep_dir = join(olmac_dir, 'dataprep')
 pcell_dir = join(olmac_dir, 'olmac_pcells')
+os.environ['KLAYOUT_HOME'] = klayout_dot_config_dir
